81831edb7552afc280dd6c7b8c427031d2ba15a3
Ridha Chowdhury <chowdhuryridha@gmail.com>
2023-07-16 00:09:49 -0500

    Roboto Slab

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
@@ -6,24 +6,8 @@ import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 import { SectionWrapper } from '../hoc';
 import SectionComponent from './SectionComponent';
-import { Canvas } from "@react-three/fiber";
-import { Text } from "@react-three/drei";
 
-function ExtrudedText({ text }) {
-  return (
-    <Text
-      color="black" // default
-      anchorX="center" // default
-      anchorY="middle" // default
-      fontSize={1}
-      position={[0, 0, 0]}
-      depth={0.5}
-    >
-      {text}
-    </Text>
-  );
-}
-
+import { Canvas, useLoader } from '@react-three/fiber'
 const About = () => {
   return (
     <>
@@ -37,13 +21,8 @@ const About = () => {
         variants={fadeIn("", "", 0.5, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a versatile full-stack developer experienced in the MERN stack and ASP.NET. With proficiency in AI tools like Langchain and text embeddings, I bring complexity and sophistication to my work. Combining aesthetics, functionality, and scalability allows me to create robust and powerful applications. My backgrounds in game development, teaching, and content creation adds creativity, pedagogical, and communicative skills to my work. Crafting immersive user experiences and optimizing systems with AI. I deliver exceptional results that inspire innovation.
+        I'm a versatile full-stack developer experienced in the MERN stack and ASP.NET. With proficiency in AI technologies like Langchain and text embeddings, I bring complexity and sophistication to my work. Combining aesthetics, functionality, and scalability allows me to create robust and powerful applications. My backgrounds in game development, teaching, and content creation adds creativity, pedagogical, and communicative skills to my work. Crafting immersive user experiences and optimizing systems with AI. I deliver exceptional results that inspire innovation.
       </motion.p>
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <ExtrudedText text="🙂 Hello World!" />
-      </Canvas>
     </>
   );
 }
