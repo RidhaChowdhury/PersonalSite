3836ccd5bb41dfb8d089a54b6f705b7994a54549
Ridha Chowdhury <chowdhuryridha@gmail.com>
2023-10-10 14:30:01 -0400

    Decrease shape count

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useState, useRef } from 'react';
+import { Suspense, useState, useRef, useMemo } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial, Preload} from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
@@ -6,20 +6,20 @@ import { styles } from '../../styles';
 
 const Stars = (props) => {
   const ref = useRef();
-  const sphere = random.inSphere(new Float32Array(5000), {radius: 1.2})
+  const sphere = random.inSphere(new Float32Array(2500), {radius: 1})
   
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
   })
-  
+
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref = {ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color='#c36312'
-          size={0.002}
+          size={0.005}
           sizeAttenuation={true}
           depthWrite={false}
         />
--- src/constants/index.js
+++ src/constants/index.js
@@ -110,56 +110,20 @@ import {
   
   const technologies = [
     {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
+      name: "React JS",
+      icon: reactjs,
     },
     {
       name: "TypeScript",
       icon: typescript,
     },
     {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
       name: "Tailwind CSS",
       icon: tailwind,
     },
     {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
       name: "Three JS",
       icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
     },
   ];
   
