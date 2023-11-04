1fa5d559cf6159465490bbd0d6c84face7684a0a
Ridha Chowdhury <chowdhuryridha@gmail.com>
2023-11-04 13:48:53 -0400

    Mobile sizes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -43,7 +43,7 @@ const ServiceCard = ({ index, title, icon, skills, transitionDelay, transitionGa
   const skillsAnimation = {
     initial: {
       scale: 1,
-      translateY: 75,
+      translateY: 125,
     },
     hover: {
       scale: 1,
@@ -52,43 +52,46 @@ const ServiceCard = ({ index, title, icon, skills, transitionDelay, transitionGa
   };
   
   return (
-    <motion.div variants={fadeIn("right", "spring", transitionDelay + (index * transitionGap), transitionDuration)}>
-      <div className='xs:w-[250px] w-full'>
-        <div className='w-full orange-teal-gradient p-[2px] rounded-[20px] shadow-card'>
-            <motion.div
-              className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col overflow-hidden'
-              whileHover='hover'
-              initial='initial'
-              variants={cardAnimation}
-              transition={{duration: 0.3}}
+    <motion.div 
+      className="flex justify-center" // Center the card container
+      variants={fadeIn("right", "spring", transitionDelay + (index * transitionGap), transitionDuration)}
+    >
+      <div className='w-60 h-80 sm:w-72 sm:h-96 md:w-80 md:h-[22rem]'> {/* Adjusted for responsiveness */}
+        <div className='w-full h-full orange-teal-gradient p-[2px] rounded-[20px] shadow-card'>
+          <motion.div
+            className='bg-tertiary rounded-[20px] py-5 px-5 flex justify-evenly items-center flex-col overflow-hidden h-full' // Ensure the height fills the container
+            whileHover='hover'
+            initial='initial'
+            variants={cardAnimation}
+            transition={{ duration: 0.3 }}
+          >
+            <motion.img
+              src={icon}
+              alt={title}
+              className='w-16 h-16 object-contain'
+              variants={shadowlessContentAnimation}
+            />
+            <motion.h3
+              className='text-white text-[20px] font-bold text-center select-none'
+              variants={headerAnimation}
             >
-              <motion.img
-                src={icon}
-                alt={title}
-                className='w-16 h-16 object-contain'
-                variants={shadowlessContentAnimation}
-              />
-              <motion.h3
-                className='text-white text-[20px] font-bold text-center'
-                variants={headerAnimation}
-              >
-                {title}
-              </motion.h3>
-              <motion.div className='flex flex-row gap-2' variants={skillsAnimation}>
-                {skills.map((skill) => (
-                  <p
-                    key={`${name}-${skill.name}`}
-                    className={`text-[16px] font-semibold ${skill.color}`}
-                  >
-                    #{skill.name}
-                  </p>
-                ))}
-              </motion.div>
+              {title}
+            </motion.h3>
+            <motion.div className='flex flex-row gap-2' variants={skillsAnimation}>
+              {skills.map((skill) => (
+                <p
+                  key={`${name}-${skill.name}`}
+                  className={`text-[16px] font-semibold select-none ${skill.color}`}
+                >
+                  #{skill.name}
+                </p>
+              ))}
             </motion.div>
-          </div>
+          </motion.div>
+        </div>
       </div>
     </motion.div>
-  );
+  );  
 };
 
 const Skills = () => {
