0f75ebc0328c82ba35c0519fcd1e362e3f2d8587
Ridha Chowdhury <chowdhuryridha@gmail.com>
2023-11-20 20:50:59 -0500

    dynamic dot backgrounds

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -2,6 +2,8 @@ import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
+import Box from "./Box";
+import GridCanvas from "./Grid";
 import HeroShapes from "./Shapes";
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, HeroShapes };
+export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, Box, GridCanvas, HeroShapes };
