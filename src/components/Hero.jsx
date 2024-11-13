25c6c27e090004fab093b7a3879012bc265fe074
Ridha Chowdhury <chowdhuryridha@gmail.com>
2024-11-12 22:01:30 -0500

    Resume update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,65 +39,85 @@ const Hero = () => {
   }, []);
   
   return (
-      <section className='relative w-full h-screen mx-auto'>
-        <HeroShapes screenWidth={window.innerWidth}/>
-        <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+    <section className="relative w-full h-screen mx-auto">
+      <HeroShapes screenWidth={window.innerWidth} />
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
         <div className="hidden sm:block">
-          <h1 className={`${styles.heroHeadText}`}>
-            Ridha Chowdhury
-          </h1>
+          <h1 className={`${styles.heroHeadText}`}>Ridha Chowdhury</h1>
           <h1 className={`${styles.heroSubText} mt-2 text-accent`}>
-            {'<Software Engineer/>'}
+            {"<Software Engineer/>"}
           </h1>
           <div className="flex mt-4">
-            <a href="https://www.linkedin.com/in/ridha-chowdhury/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.linkedin.com/in/ridha-chowdhury/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="w-10 h-10 mr-2">
                 <img src={linkedin} alt="LinkedIn" className="w-full h-full" />
               </button>
             </a>
-            <a href="https://github.com/RidhaChowdhury" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/RidhaChowdhury"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <button className="w-10 h-10 mr-2">
                 <img src={github_mark} alt="GitHub" className="w-full h-full" />
               </button>
             </a>
-            <a href={resume} target="_blank" rel="noopener noreferrer" className="inline-flex items-center space-x-2">
-              <button className="inline-flex flex-start items-center justify-center bg-accent hover:bg-accent/75 text-white font-bold py-2 px-4 rounded h-10">
-                <img src={resume_icon} alt="Resume" className="w-8 h-8 m-0 p-0" />
-                <h2 className={`${styles.heroResume} mt-0 text-sm md:text-base lg:text-lg`}>Resume</h2>
+            <a href={resume} target="_blank" rel="noopener noreferrer">
+              <button className="w-10 h-10 mr-2">
+                <img src={resume_icon} alt="Resume" className="w-full h-full" />
               </button>
             </a>
           </div>
         </div>
         <div className="sm:hidden">
-          <h1 className={`${styles.mobile.heroHeadText}`}>
-            Ridha
-          </h1>
+          <h1 className={`${styles.mobile.heroHeadText}`}>Ridha</h1>
           <h1 className={`${styles.mobile.heroSubText} mt-2 text-accent`}>
-            {'<SDE/>'}
+            {"<SDE/>"}
           </h1>
-          <a href="https://www.linkedin.com/in/ridha-chowdhury/" target="_blank" rel="noopener noreferrer">
-              <button className="w-10 h-10 mr-2">
-                <img src={linkedin} alt="LinkedIn" className="w-full h-full" />
-              </button>
-            </a>
-            <a href="https://github.com/your-profile" target="_blank" rel="noopener noreferrer">
-              <button className="w-10 h-10 mr-2">
-                <img src={github_mark} alt="GitHub" className="w-full h-full" />
-              </button>
-            </a>
-            <a href={resume} target="_blank" rel="noopener noreferrer" className="inline-flex items-center space-x-2">
-              <button className="inline-flex flex-start items-center justify-center bg-accent hover:bg-accent/75 text-white font-bold py-2 px-4 rounded h-10">
-                <img src={resume_icon} alt="Resume" className="w-8 h-8 m-0 p-0" />
-                <h2 className={`${styles.heroResume} mt-0 text-sm md:text-base lg:text-lg`}>Resume</h2>
-              </button>
-            </a>
-          <div className="flex mt-4">
-          </div>
+          <a
+            href="https://www.linkedin.com/in/ridha-chowdhury/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="w-10 h-10 mr-2">
+              <img src={linkedin} alt="LinkedIn" className="w-full h-full" />
+            </button>
+          </a>
+          <a
+            href="https://github.com/your-profile"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="w-10 h-10 mr-2">
+              <img src={github_mark} alt="GitHub" className="w-full h-full" />
+            </button>
+          </a>
+          <a
+            href={resume}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-flex items-center space-x-2"
+          >
+            <button className="inline-flex flex-start items-center justify-center bg-accent hover:bg-accent/75 text-white font-bold py-2 px-4 rounded h-10">
+              <img src={resume_icon} alt="Resume" className="w-8 h-8 m-0 p-0" />
+              <h2
+                className={`${styles.heroResume} mt-0 text-sm md:text-base lg:text-lg`}
+              >
+                Resume
+              </h2>
+            </button>
+          </a>
+          <div className="flex mt-4"></div>
         </div>
       </div>
 
-
-        {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
           <a href="#about">
             <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
               <motion.div
@@ -114,7 +134,7 @@ const Hero = () => {
             </div>
           </a>
         </div> */}
-      </section>
+    </section>
   );
 }
 
